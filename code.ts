f0735114554951c5afb68b5bbfad20cff509436d
alessandro.mastro@icloud.com <alessandro.mastro@icloud.com>
2023-04-21 09:34:55 +0200

    Environments

--- code.ts
+++ code.ts
@@ -1,5 +1,4 @@
 figma.showUI(__html__);
-
 figma.ui.resize(500, 500);
 
 const savedClasses: { [key: string]: object } = JSON.parse(figma.root.getPluginData('savedClasses') || '{}');
@@ -57,6 +56,27 @@ function applyDesignProperties(node: FrameNode, properties: any) {
   node.verticalPadding = properties.verticalPadding;
 }
 
+function isValidSelection(): boolean {
+  const nodes = figma.currentPage.selection;
+  return nodes.length === 1 && nodes[0].type === 'FRAME';
+}
+
+figma.on('selectionchange', () => {
+  figma.ui.postMessage({
+    type: 'display-saved-classes',
+    savedClasses,
+    savedClassNames,
+    validSelection: isValidSelection(),
+  });
+});
+
+figma.ui.postMessage({
+  type: 'display-saved-classes',
+  savedClasses,
+  savedClassNames,
+  validSelection: isValidSelection(),
+});
+
 figma.ui.onmessage = async (msg) => {
   if (msg.type === 'save-class') {
     const nodes = figma.currentPage.selection;
@@ -68,44 +88,71 @@ figma.ui.onmessage = async (msg) => {
       if (!savedClassNames.includes(className)) {
         savedClassNames.push(className);
       }
-      figma.root.setPluginData('savedClasses', JSON.stringify(savedClasses));
-      figma.root.setPluginData('savedClassNames', JSON.stringify(savedClassNames));
-      figma.ui.postMessage({ type: 'display-saved-classes', savedClasses, savedClassNames });
-    } else {
-      figma.notify('Please select a single frame to save the class.');
-    }
-  } else if (msg.type === 'delete-class') {
-    delete savedClasses[msg.className];
-    const index = savedClassNames.indexOf(msg.className);
-    if (index > -1) {
-      savedClassNames.splice(index, 1);
-    }
-    figma.root.setPluginData('savedClasses', JSON.stringify(savedClasses));
-    figma.root.setPluginData('savedClassNames', JSON.stringify(savedClassNames));
-    figma.ui.postMessage({ type: 'display-saved-classes', savedClasses, savedClassNames });
-  } else if (msg.type === 'apply-class') {
-    const nodes = figma.currentPage.selection;
-    if (nodes.length === 1 && nodes[0].type === 'FRAME') {
-      const frame = nodes[0] as FrameNode;
-      applyDesignProperties(frame, savedClasses[msg.className]);
-    } else {
-      figma.notify('Please select a single frame to apply the class.');
-    }
-  } else if (msg.type === 'rename-class') {
-    const oldClassName = msg.className;
-    const newClassName = msg.newClassName;
-    savedClasses[newClassName] = savedClasses[oldClassName];
-    delete savedClasses[oldClassName];
-    const index = savedClassNames.indexOf(oldClassName);
-    if (index > -1) {
-      savedClassNames[index] = newClassName;
-    }
-    figma.root.setPluginData('savedClasses', JSON.stringify(savedClasses));
-    figma.root.setPluginData('savedClassNames', JSON.stringify(savedClassNames));
-    figma.ui.postMessage({ type: 'display-saved-classes', savedClasses, savedClassNames });
-  } else if (msg.type === 'empty-class-name') {
-    figma.notify('Class name cannot be empty.');
-  }
-};
-
-     
+      figma.root.setPluginData('saved
+      function handleSaveClass() {
+        const className = inputEl.value.trim();
+        if (!className) {
+          figma.ui.postMessage({ type: 'empty-class-name' });
+          return;
+        }
+        const msg = { type: 'save-class', className };
+        figma.ui.postMessage(msg);
+        inputEl.value = '';
+        successMessageEl.style.opacity = '1';
+        setTimeout(() => {
+          successMessageEl.style.opacity = '0';
+        }, 1500);
+      }
+      function handleDeleteClass(className) {
+        const msg = { type: 'delete-class', className };
+        figma.ui.postMessage(msg);
+      }
+      
+      function handleApplyClass(className) {
+        const msg = { type: 'apply-class', className };
+        figma.ui.postMessage(msg);
+      }
+      
+      function handleRenameClass(oldClassName, newClassName) {
+        const msg = { type: 'rename-class', className: oldClassName, newClassName };
+        figma.ui.postMessage(msg);
+      }
+      
+      function handleInputKeypress(e) {
+        if (e.keyCode === 13) {
+          handleSaveClass();
+        }
+      }
+      
+      function init() {
+        formEl.addEventListener('submit', (e) => e.preventDefault());
+        saveClassBtnEl.addEventListener('click', handleSaveClass);
+        inputEl.addEventListener('keypress', handleInputKeypress);
+      
+        savedClassesEl.addEventListener('click', (e) => {
+          const target = e.target;
+          if (target.tagName === 'BUTTON') {
+            const action = target.dataset.action;
+            const className = target.dataset.className;
+            if (action === 'delete') {
+              handleDeleteClass(className);
+            } else if (action === 'apply') {
+              handleApplyClass(className);
+            }
+          }
+        });
+      
+        savedClassesEl.addEventListener('dblclick', (e) => {
+          const target = e.target;
+          if (target.tagName === 'SPAN') {
+            const className = target.dataset.className;
+            const newClassName = prompt('Enter a new name for the class', className);
+            if (newClassName && newClassName !== className) {
+              handleRenameClass(className, newClassName);
+            }
+          }
+        });
+      }
+      
+      init();
+      
