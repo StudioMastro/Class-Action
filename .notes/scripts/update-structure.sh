e2c31b5d8f6c9767990e5d9eb4258cd873c038a7
Mastro <alessandro.mastro@icloud.com>
2025-01-09 22:18:36 +0100

    refactor: semplificazione script directory structure

--- .notes/scripts/update-structure.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get the directory where the script is located
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Run the shell script
-"$SCRIPT_DIR/update_directory.sh"
-
-echo "Directory structure has been updated!" 
